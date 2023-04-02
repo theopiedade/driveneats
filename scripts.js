5ba115f420d104cea060302ce8a770321d1baef4
theopiedade <theopiedade@hotmail.com>
2023-04-02 16:21:46 -0300

    selects 1/2 complete

--- scripts.js
+++ scripts.js
@@ -1,13 +1,23 @@
 function select(type,item,check) {
  
-  const s = document.querySelector(type+" .selected");
-  if (s !== null) { 
-    s.classList.remove('selected');
+  const s1 = document.querySelector(type+" .selected");
+  if (s1 !== null) { 
+    s1.classList.remove('selected');
   }
   
-  const i = document.querySelector(item);
-  if (s !== i) {
-    i.classList.toggle('selected');
+  const s2 = document.querySelector(item);
+  if (s2 !== s1) {
+    s2.classList.toggle('selected');
+  }
+  
+  const c1 = document.querySelector(type+" .checked");
+  if (c1 !== null) { 
+    c1.classList.remove('checked');
+  }
+  
+  const c2 = document.querySelector(check);
+  if (c2 !== c1) {
+    c2.classList.toggle('checked');
   }
 
 }
